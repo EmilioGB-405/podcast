ae5b45a203b838286f9822ae301e1700582a978d
Emilio <EmilioGB117@outlook.com>
2025-05-22 17:10:56 -0600

    eliminado js app1 y podcast y guardando cambios en index html y app js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 
 import * as THREE from 'three';
 
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+/* import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; */
 
 
 const scene = new THREE.Scene();
 
-console.log(scene)
+/* console.log(scene) */
 
 const camara = new THREE.PerspectiveCamera(75, window.innerWidth, window.innerHeight, 0.1, 1000 );
 camara.position.z =3;
@@ -16,30 +16,7 @@ camara.position.z =3;
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('myCanvas')});
 renderer.setSize(window.innerWidth, window.innerHeight);
 /* document.body.appendChild(renderer.domElement) */
-
-
-const loader = new GLTFLoader();
-loader.load('/img/hologramamicrophone.glb', (gltf)=>{
-    const model = gltf.scene;
-    scene.add(model);
-
-    model.scale.set(4.5,4.5,4.5);
-    model.rotation.y = Math.PI;
-
-       const animate = () =>{
-        requestAnimationFrame(animate)
-        model.rotation.y += 0.01;
-        renderer.render(scene, camara)
-    };
-    animate();
-
-}, undefined, (error) => {
-    console.error('Error al cargar el modelo', error)
-
-});
-
  
-
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2,2,5);
 scene.add(light);
@@ -51,3 +28,5 @@ window.addEventListener('resize', ()=>{
     camara.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+export{camara, scene, renderer}
