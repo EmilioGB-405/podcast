1298172b1ed2bb07819f2484de03880c219ab788
Ignacio Gómez <ignaciogb900@gmail.com>
2025-05-23 15:41:40 -0600

    cambios en css html y un nuevo archivo js

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+const left = document.querySelector('#left');
+  const right = document.querySelector('#right');
+
+  function updateCardClasses() {
+    const cards = document.querySelectorAll('.cardSlider');
+
+    cards.forEach(card => {
+      card.classList.remove('card-left', 'card-center', 'card-right');
+    });
+
+    if (cards[0]) cards[0].classList.add('card-left');
+    if (cards[1]) cards[1].classList.add('card-center');
+    if (cards[2]) cards[2].classList.add('card-right');
+  }
+
+  updateCardClasses();
+
+  right.addEventListener('click', () => {
+    const cards = document.querySelectorAll('.cardSlider');
+    document.querySelector('#cardContentComments').appendChild(cards[0]);
+    updateCardClasses();
+  });
+
+  left.addEventListener('click', () => {
+    const cards = document.querySelectorAll('.cardSlider');
+    document.querySelector('#cardContentComments').prepend(cards[cards.length - 1]);
+    updateCardClasses();
+  });
