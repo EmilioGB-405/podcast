25e402ba93f23f312f98b5bab2160299ad3c6409
Emilio <EmilioGB117@outlook.com>
2025-05-26 13:20:50 -0600

    guardando cambios en el documento de js

--- js/microphone.js
+++ js/microphone.js
@@ -4,35 +4,6 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 import {scene,camara,renderer} from "./app";
 
-const BODY_ELEMENT = document.querySelector('body');
-const GETCHILDREN = BODY_ELEMENT.children
-let scrollPosition = BODY_ELEMENT.scrollTop
-
-console.log(GETCHILDREN);
-console.log(BODY_ELEMENT);
-
-function scrollAnimation(){
-    BODY_ELEMENT.addEventListener('scroll', ()=>{
-        GETCHILDREN.forEach(scrollPosition => {
-           if(scrollPosition === 1){
-            console.log('position 1')
-           }
-        });
-    })
-    window.requestAnimationFrame(()=>{
-        console.log(scrollPosition)
-    })
-}
-scrollAnimation()
-
-
-/* for(let i = 1; i < GETCHILDREN.length; i+=2){
-    const CHILD = GETCHILDREN[i]
-    console.log(CHILD);
-
-} */
-
-
 const holograma = new GLTFLoader();
 holograma.load('/img/hologramamicrophone.glb', (gltf)=>{
     const model = gltf.scene;
