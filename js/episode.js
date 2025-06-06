0bd45cf06de388a7f69d416fd0dc24435c7050bf
Ignacio Gómez <ignaciogb900@gmail.com>
2025-06-06 10:22:42 -0600

    nuevas imagenes y cambios en todods los documentos episodes

--- js/episode.js
+++ js/episode.js
@@ -5,232 +5,242 @@ let episodes = {
             number: "Episode 1",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeOne.jpg"
+            img: "img/episodeOne.jpg",
+            year: "2023"
 
         },
          {
             number: "Episode 2",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTwo.jpg"
-
+            img: "img/episodeTwo.jpg",
+            year: "2023"
         },
          {
             number: "Episode 3",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThree.jpg"
-
+            img: "img/episodeThree.jpg",
+            year: "2023"
         },
          {
             number: "Episode 4",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeFour.jpg"
-
+            img: "img/episodeFour.jpg",
+            year: "2023"
+            
         },
          {
             number: "Episode 5",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeFive.jpg"
+            img: "img/episodeFive.jpg",
+            year: "2023"
 
         },
          {
             number: "Episode 6",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeSixe.jpg"
-
+            img: "img/episodeSixe.jpg",
+            year: "2023"
         },
          {
             number: "Episode 7",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeSeven.jpg"
-
+            img: "img/episodeSeven.jpg",
+            year: "2023",
         },
          {
             number: "Episode 8",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeEight.jpg"
-
+            img: "img/episodeEight.jpg",
+            year: "2023"
         },
          {
             number: "Episode 9",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeNine.jpg"
-
+            img: "img/episodeNine.jpg",
+            year: "2023"
         },
          {
             number: "Episode 10",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTen.jpg"
-
+            img: "img/episodeTen.jpg",
+            year: "2023"
         },
          {
             number: "Episode 11",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeEleven.jpg"
-
+            img: "img/episodeEleven.jpg",
+            year: "2023"
         },
          {
             number: "Episode 12",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTwelve.jpg"
-
+            img: "img/episodeTwelve.jpg",
+            year: "2023"
         },
          {
             number: "Episode 13",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThirteen.jpg"
-
+            img: "img/episodeThirteen.jpg",
+            year: "2023"
         },
          {
             number: "Episode 14",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeFourteen.jpg"
+            img: "img/episodeFourteen.jpg",
+            year: "2023"
 
         },
          {
             number: "Episode 15",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodefifteen.jpg"
-
+            img: "img/episodefifteen.jpg",
+            year: "2023"
         },
          {
             number: "Episode 16",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeSexteen.jpg"
+            img: "img/episodeSexteen.jpg",
+            year: "2024"
 
         },
          {
             number: "Episode 17",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeSeventeen.jpg"
-
+            img: "img/episodeSeventeen.jpg",
+            year: "2024"
         },
          {
             number: "Episode 18",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeEighteen.jpg"
-
+            img: "img/episodeEighteen.jpg",
+            year: "2024"
         },
          {
             number: "Episode 19",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeNinteen.jpg"
-
+            img: "img/episodeNinteen.jpg",
+            year: "2024"
         },
          {
             number: "Episode 20",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewnty.jpg"
+            img: "img/episodeTewnty.jpg",
+            year: "2024"
 
         },
          {
             number: "Episode 21",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyOne.jpg"
+            img: "img/episodeTewntyOne.jpg",
+            year: "2024"
 
         },
          {
             number: "Episode 22",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyTwo.jpg"
-
+            img: "img/episodeTewntyTwo.jpg",
+            year: "2024"
         },
          {
             number: "Episode 23",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyThree.jpg"
+            img: "img/episodeTewntyThree.jpg",
+            year: "2024"
 
         },
          {
             number: "Episode 24",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyFourjpg"
-
+            img: "img/episodeTewntyFourjpg",
+            year: "2024"
         },
         {
             number: "Episode 25",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyFive.jpg"
-
+            img: "img/episodeTewntyFive.jpg",
+            year: "2024"
         },
         {
             number: "Episode 26",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntySixe.jpg"
-
+            img: "img/episodeTewntySixe.jpg",
+            year: "2024"
         },
         {
             number: "Episode 27",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntySeven.jpg"
-
+            img: "img/episodeTewntySeven.jpg",
+            year: "2024"
         },
         {
             number: "Episode 28",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyNightEight.jpg"
-
+            img: "img/episodeTewntyNightEight.jpg",
+            year: "2024"
         },
         {
             number: "Episode 29",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyNine.jpg"
+            img: "img/episodeTewntyNine.jpg",
+            year: "2025"
 
         },
         {
             number: "Episode 30",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThirty.jpg"
+            img: "img/episodeThirty.jpg",
+            year: "2025"
 
         },
          {
             number: "Episode 31",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThirtyOne.jpg"
-
+            img: "img/episodeThirtyOne.jpg",
+            year: "2025"
         },
          {
             number: "Episode 32",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThirtyTwo.jpg"
-
+            img: "img/episodeThirtyTwo.jpg",
+            year: "2025"
         },
          {
             number: "Episode 33",
             tittle: "",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeThirtyThree.jpg"
-
+            img: "img/episodeThirtyThree.jpg",
+            year: "2025"
         },
     ]
 }
