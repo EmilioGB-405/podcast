6d11acaa1e416735f220d0cea18f6b2c15887cff
Ignacio Gómez <ignaciogb900@gmail.com>
2025-06-09 15:34:11 -0600

    cambios en general y episode

--- js/episode.js
+++ js/episode.js
@@ -1,246 +1,324 @@
 'use strict'
-let episodes = {
-    episode: [
+let episodes = [
         {
             number: "Episode 1",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeOne.jpg",
+            imgCell: "img/episodeOneCell.jpg",
             year: "2023"
-
         },
-         {
+        {
             number: "Episode 2",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTwo.jpg",
+            imgCell: "img/episodeTwoCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 3",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThree.jpg",
+            imgCell: "img/episodeThreeCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 4",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeFour.jpg",
+            imgCell: "img/episodeFourCell.jpg",
             year: "2023"
-            
         },
-         {
+        {
             number: "Episode 5",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeFive.jpg",
+            imgCell: "img/episodeFiveCell.jpg",
             year: "2023"
-
         },
-         {
+        {
             number: "Episode 6",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeSixe.jpg",
+            imgCell: "img/episodeSixeCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 7",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeSeven.jpg",
-            year: "2023",
+            imgCell: "img/episodeSevenCell.jpg",
+            year: "2023"
         },
-         {
+        {
             number: "Episode 8",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeEight.jpg",
+            imgCell: "img/episodeEightCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 9",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeNine.jpg",
+            imgCell: "img/episodeNineCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 10",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTen.jpg",
+            imgCell: "img/episodeTenCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 11",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeEleven.jpg",
+            imgCell: "img/episodeElevenCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 12",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTwelve.jpg",
+            imgCell: "img/episodeTwelveCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 13",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThirteen.jpg",
+            imgCell: "img/episodeThirteenCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 14",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeFourteen.jpg",
+            imgCell: "img/episodeFourteenCell.jpg",
             year: "2023"
-
         },
-         {
+        {
             number: "Episode 15",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodefifteen.jpg",
+            imgCell: "img/episodefifteenCell.jpg",
             year: "2023"
         },
-         {
+        {
             number: "Episode 16",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeSexteen.jpg",
+            imgCell: "img/episodeSexteenCell.jpg",
             year: "2024"
-
         },
-         {
+        {
             number: "Episode 17",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeSeventeen.jpg",
+            imgCell: "img/episodeSeventeenCell.jpg",
             year: "2024"
         },
-         {
+        {
             number: "Episode 18",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeEighteen.jpg",
+            imgCell: "img/episodeEighteenCell.jpg",
             year: "2024"
         },
-         {
+        {
             number: "Episode 19",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeNinteen.jpg",
+            imgCell: "img/episodeNinteenCell.jpg",
             year: "2024"
         },
-         {
+        {
             number: "Episode 20",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewnty.jpg",
+            imgCell: "img/episodeTewntyCell.jpg",
             year: "2024"
-
         },
-         {
+        {
             number: "Episode 21",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyOne.jpg",
+            imgCell: "img/episodeTewntyOneCell.jpg",
             year: "2024"
-
         },
-         {
+        {
             number: "Episode 22",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyTwo.jpg",
+            imgCell: "img/episodeTewntyTwoCell.jpg",
             year: "2024"
         },
-         {
+        {
             number: "Episode 23",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyThree.jpg",
+            imgCell: "img/episodeTewntyThreeCell.jpg",
             year: "2024"
-
         },
-         {
+        {
             number: "Episode 24",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
-            img: "img/episodeTewntyFourjpg",
+            information: "Lorem ipsum...",
+            img: "img/episodeTewntyFour.jpg",
+            imgCell: "img/episodeTewntyFourCell.jpg",
             year: "2024"
         },
         {
             number: "Episode 25",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyFive.jpg",
+            imgCell: "img/episodeTewntyFiveCell.jpg",
             year: "2024"
         },
         {
             number: "Episode 26",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntySixe.jpg",
+            imgCell: "img/episodeTewntySixeCell.jpg",
             year: "2024"
         },
         {
             number: "Episode 27",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntySeven.jpg",
+            imgCell: "img/episodeTewntySevenCell.jpg",
             year: "2024"
         },
         {
             number: "Episode 28",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyNightEight.jpg",
+            imgCell: "img/episodeTewntyNightEightCell.jpg",
             year: "2024"
         },
         {
             number: "Episode 29",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeTewntyNine.jpg",
+            imgCell: "img/episodeTewntyNineCell.jpg",
             year: "2025"
-
         },
         {
             number: "Episode 30",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThirty.jpg",
+            imgCell: "img/episodeThirtyCell.jpg",
             year: "2025"
-
         },
-         {
+        {
             number: "Episode 31",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThirtyOne.jpg",
+            imgCell: "img/episodeThirtyOneCell.jpg",
             year: "2025"
         },
-         {
+        {
             number: "Episode 32",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThirtyTwo.jpg",
+            imgCell: "img/episodeThirtyTwoCell.jpg",
             year: "2025"
         },
-         {
+        {
             number: "Episode 33",
             tittle: "",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+            information: "Lorem ipsum...",
             img: "img/episodeThirtyThree.jpg",
+            imgCell: "img/episodeThirtyThreeCell.jpg",
             year: "2025"
-        },
-    ]
-}
+        }
+];
+let reversedEpisodes = episodes.reverse();
+
+const cards = document.querySelectorAll("#episodes .card");
+
+Array.from(cards).forEach((card, index) => {
+  const episode = reversedEpisodes[index];
+  if (!episode) return; // Evita errores si hay más tarjetas que datos
+
+  const imagenCell = card.querySelector(".imagenCell");
+  const imagenCompu = card.querySelector(".imagenCompu");
+  const img = card.querySelector("img");
+  const title = card.querySelector(".title");
+  const text = card.querySelector(".text");
+
+  imagenCell.srcset = episode.imgCell;
+  imagenCompu.srcset = episode.img;
+  img.src = episode.img; // fallback
+  img.alt = episode.imageAlt;
+
+  title.textContent = episode.number;
+  text.textContent = episode.information;
+
+ 
+});
+let right = document.querySelector('#right');
+
+
+let i = 0; // Mueve esto fuera del event listener para mantener el índice global
+
+right.addEventListener('click', () => {
+    i = (i + 1) % Math.ceil(reversedEpisodes.length / cards.length); // índice de página
+
+    Array.from(cards).forEach((card, index) => {
+        const episodeIndex = i * cards.length + index;
+        const episode = reversedEpisodes[episodeIndex];
+        if (!episode) {
+            card.style.display = "none"; // Oculta si no hay episodio
+            return;
+        }
+
+        card.style.display = ""; // Asegura que se muestre si hay contenido
+
+        const imagenCell = card.querySelector(".imagenCell");
+        const imagenCompu = card.querySelector(".imagenCompu");
+        const img = card.querySelector("img");
+        const title = card.querySelector(".title");
+        const text = card.querySelector(".text");
+
+        imagenCell.srcset = episode.imgCell;
+        imagenCompu.srcset = episode.img;
+        img.src = episode.img;
+        img.alt = episode.imageAlt || "";
+
+        title.textContent = episode.number;
+        text.textContent = episode.information;
+    });
+});
