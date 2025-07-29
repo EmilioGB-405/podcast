b6497d72eda63713334bac2e6d59c2b92772e908
Emilio <EmilioGB117@outlook.com>
2025-07-29 14:34:13 -0600

    agregando nuevas carpetas y modificando algunos archivos js html

--- js/blog.js
+++ js/blog.js
@@ -5,26 +5,26 @@ let global = {
     {
       newsTitle: "AI Tutors Transform Virtual Learning",
       information: "AI-based tutors now personalize lessons for students...",
-      img: "./img/AiNewOne.jpg",
-      imgCell: "./img/AiNewOneCell.jpg"
+      img: "../img/AiNewOne.jpg",
+      imgCell: "../img/AiNewOneCell.jpg"
     },
     {
       newsTitle: "Cyberattack on Grid Blocked by AI",
       information: "European power grids were targeted by hackers...",
-      img: "./img/cyberSecurityOne.jpg",
-      imgCell: "./img/cyberSecurityOneCell.jpg"
+      img: "../img/cyberSecurityOne.jpg",
+      imgCell: "../img/cyberSecurityOneCell.jpg"
     },
     {
       newsTitle: "Quantum Chip Solves Logistics Problem",
       information: "A new quantum chip optimized global shipping routes...",
-      img: "./img/quantumOne.jpg",
-      imgCell: "./img/quantumOneCell.jpg"
+      img: "../img/quantumOne.jpg",
+      imgCell: "../img/quantumOneCell.jpg"
     },
     {
       newsTitle: "AI and Jobs",
       information: "AI cuts routine jobs but boosts efficiency...",
-      img: "./img/economyOne.jpg",
-      imgCell: "./img/economyOneCellphone.jpg"
+      img: "../img/economyOne.jpg",
+      imgCell: "../img/economyOneCellphone.jpg"
     }
   ],
   index: 0,
