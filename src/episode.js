6775ef6734170ff389947bfabe746c54ef315cf1
Ignacio Gómez <ignaciogb900@gmail.com>
2025-07-30 17:12:54 -0600

    js episode

--- src/episode.js
+++ src/episode.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 let global = {
   episodes: [
     {
       number: "Episode 1",
       tittle: "",
-      information: "In this very first episode of TechPulse, we dive into the fascinating world of digital connections powering the internet, artificial intelligence, and neural networks.",
+      information: "In this very first episode of TechPulse...",
       img: "../img/episodeOne.jpg",
       imgCell: "../img/episodeOneCell.jpg",
-      url:"https://www.youtube.com/watch?v=Gi8LUnhP5yU",
+      url: "https://www.youtube.com/watch?v=Gi8LUnhP5yU",
       year: "2023"
     },
     {
       number: "Episode 2",
       tittle: "",
-      information: "Unlock the incredible potential of merging human biology with advanced technology...",
+      information: "Unlock the incredible potential of merging human biology...",
       img: "../img/episodeTwo.jpg",
       imgCell: "../img/episodeTwoCell.jpg",
       url: "https://www.youtube.com/watch?v=piHkfmeU7Wo",
@@ -31,263 +31,119 @@ let global = {
     {
       number: "Episode 4",
       tittle: "",
-      information: "A deep dive into digital manufacturing and 3D printing...",
+      information: "Explore the impact of global connectivity...",
       img: "../img/episodeFour.jpg",
       imgCell: "../img/episodeFourCell.jpg",
-      url: "https://www.youtube.com/watch?v=j4aw7mI5w4o",
+      url: "https://www.youtube.com/watch?v=lMEzIhzyOz8",
       year: "2023"
     },
     {
       number: "Episode 5",
       tittle: "",
-      information: "Explore cutting-edge wearable devices reshaping our lives...",
+      information: "How smart cities are reshaping the future...",
       img: "../img/episodeFive.jpg",
       imgCell: "../img/episodeFiveCell.jpg",
-      url: "https://www.youtube.com/watch?v=1Bd3KfncpPQ",
+      url: "https://www.youtube.com/watch?v=VZLrZbQ6ZPQ",
       year: "2023"
     },
     {
       number: "Episode 6",
       tittle: "",
-      information: "Dive into blockchain and the future of secure digital transactions.",
+      information: "The rise of wearable tech and its applications...",
       img: "../img/episodeSix.jpg",
       imgCell: "../img/episodeSixCell.jpg",
-      url: "https://www.youtube.com/watch?v=ywccQSPF2CU",
+      url: "https://www.youtube.com/watch?v=8xzR3G7MB5M",
       year: "2023"
     },
     {
       number: "Episode 7",
       tittle: "",
-      information: "Explore the impact of quantum computing in data processing.",
+      information: "Artificial Intelligence in education...",
       img: "../img/episodeSeven.jpg",
       imgCell: "../img/episodeSevenCell.jpg",
-      url: "https://www.youtube.com/watch?v=EMvNfL7H23U",
+      url: "https://www.youtube.com/watch?v=dClW7gsED9s",
       year: "2023"
     },
     {
       number: "Episode 8",
       tittle: "",
-      information: "Smart cities and how technology is transforming urban living.",
+      information: "Explore quantum computing fundamentals...",
       img: "../img/episodeEight.jpg",
       imgCell: "../img/episodeEightCell.jpg",
-      url: "https://www.youtube.com/watch?v=5tAUJTL3_K8",
+      url: "https://www.youtube.com/watch?v=wPH0HLg_J_o",
       year: "2023"
     },
     {
       number: "Episode 9",
       tittle: "",
-      information: "A journey into biotechnology and future medicine.",
+      information: "Drones in daily life and their regulations...",
       img: "../img/episodeNine.jpg",
       imgCell: "../img/episodeNineCell.jpg",
-      url: "https://www.youtube.com/watch?v=RR1SPaFyFJ4",
+      url: "https://www.youtube.com/watch?v=n6COYmNpHk4",
       year: "2023"
     },
     {
       number: "Episode 10",
       tittle: "",
-      information: "Discover the power of big data and predictive analytics.",
+      information: "The ethical side of AI and automation...",
       img: "../img/episodeTen.jpg",
       imgCell: "../img/episodeTenCell.jpg",
-      url: "https://www.youtube.com/watch?v=1aKICxDgbmM",
+      url: "https://www.youtube.com/watch?v=PYZKwW9WHTs",
       year: "2023"
     },
     {
       number: "Episode 11",
       tittle: "",
-      information: "Unveiling the mysteries of machine learning algorithms.",
+      information: "Space tech breakthroughs and private space travel...",
       img: "../img/episodeEleven.jpg",
       imgCell: "../img/episodeElevenCell.jpg",
-      url: "https://www.youtube.com/watch?v=6QivDFlrJcg",
+      url: "https://www.youtube.com/watch?v=ZJhU3msyZVw",
       year: "2024"
     },
     {
       number: "Episode 12",
       tittle: "",
-      information: "Exploring how drones are changing logistics and photography.",
+      information: "Green energy tech innovations...",
       img: "../img/episodeTwelve.jpg",
       imgCell: "../img/episodeTwelveCell.jpg",
-      url: "https://www.youtube.com/watch?v=Kp23RxF-7vU",
+      url: "https://www.youtube.com/watch?v=iW9cY_xvB5U",
       year: "2024"
     },
     {
       number: "Episode 13",
       tittle: "",
-      information: "A look at sustainable tech and the green revolution.",
+      information: "Augmented Reality in the workplace...",
       img: "../img/episodeThirteen.jpg",
       imgCell: "../img/episodeThirteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=z2GDhESfV64",
+      url: "https://www.youtube.com/watch?v=0BzQmdo1F54",
       year: "2024"
     },
     {
       number: "Episode 14",
       tittle: "",
-      information: "The future of robotics in everyday life.",
+      information: "How big data is shaping decision-making...",
       img: "../img/episodeFourteen.jpg",
       imgCell: "../img/episodeFourteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=wGeKDaNUN6g",
+      url: "https://www.youtube.com/watch?v=fe4O1QDN0G8",
       year: "2024"
     },
     {
       number: "Episode 15",
       tittle: "",
-      information: "Exploring the ethics of artificial intelligence.",
+      information: "Biometric technology in everyday devices...",
       img: "../img/episodeFifteen.jpg",
       imgCell: "../img/episodeFifteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=dDK0A5K_oNM",
+      url: "https://www.youtube.com/watch?v=gEp5Ez5ZqeU",
       year: "2024"
     },
     {
       number: "Episode 16",
       tittle: "",
-      information: "How virtual and augmented reality are reshaping experiences.",
+      information: "Digital privacy and surveillance in 2024...",
       img: "../img/episodeSixteen.jpg",
       imgCell: "../img/episodeSixteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=RoOK5U8z_U0",
+      url: "https://www.youtube.com/watch?v=nqAxXKkFQZY",
       year: "2024"
-    },
-    {
-      number: "Episode 17",
-      tittle: "",
-      information: "Discover innovations in nanotechnology.",
-      img: "../img/episodeSeventeen.jpg",
-      imgCell: "../img/episodeSeventeenCell.jpg",
-      url: "https://www.youtube.com/watch?v=SmPhtJCHN5U",
-      year: "2024"
-    },
-    {
-      number: "Episode 18",
-      tittle: "",
-      information: "How 5G is accelerating global connectivity.",
-      img: "../img/episodeEighteen.jpg",
-      imgCell: "../img/episodeEighteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=olB8nUQBRHE",
-      year: "2024"
-    },
-    {
-      number: "Episode 19",
-      tittle: "",
-      information: "Exploring the security challenges in digital transformation.",
-      img: "../img/episodeNineteen.jpg",
-      imgCell: "../img/episodeNineteenCell.jpg",
-      url: "https://www.youtube.com/watch?v=jYm2GMsZTCY",
-      year: "2024"
-    },
-    {
-      number: "Episode 20",
-      tittle: "",
-      information: "The impact of technology on education and e-learning.",
-      img: "../img/episodeTwenty.jpg",
-      imgCell: "../img/episodeTwentyCell.jpg",
-      url: "https://www.youtube.com/watch?v=Qemc9aHBo8c",
-      year: "2024"
-    },
-    {
-      number: "Episode 21",
-      tittle: "",
-      information: "Neuroscience meets AI: can machines think like us?",
-      img: "../img/episodeTwentyOne.jpg",
-      imgCell: "../img/episodeTwentyOneCell.jpg",
-      url: "https://www.youtube.com/watch?v=3pl3FFvVBOs",
-      year: "2024"
-    },
-    {
-      number: "Episode 22",
-      tittle: "",
-      information: "Fascinating innovations in space tech and satellites.",
-      img: "../img/episodeTwentyTwo.jpg",
-      imgCell: "../img/episodeTwentyTwoCell.jpg",
-      url: "https://www.youtube.com/watch?v=bpvFiF2F8sI",
-      year: "2024"
-    },
-    {
-      number: "Episode 23",
-      tittle: "",
-      information: "Wearables for health: the rise of bio-monitoring.",
-      img: "../img/episodeTwentyThree.jpg",
-      imgCell: "../img/episodeTwentyThreeCell.jpg",
-      url: "https://www.youtube.com/watch?v=d-wJhSEiQe0",
-      year: "2024"
-    },
-    {
-      number: "Episode 24",
-      tittle: "",
-      information: "Deepfake technology: innovation or danger?",
-      img: "../img/episodeTwentyFour.jpg",
-      imgCell: "../img/episodeTwentyFourCell.jpg",
-      url: "https://www.youtube.com/watch?v=Gd7E-QUehsU",
-      year: "2024"
-    },
-    {
-      number: "Episode 25",
-      tittle: "",
-      information: "How startups are pushing the boundaries of innovation.",
-      img: "../img/episodeTwentyFive.jpg",
-      imgCell: "../img/episodeTwentyFiveCell.jpg",
-      url: "https://www.youtube.com/watch?v=rDb4qDuZiAU",
-      year: "2024"
-    },
-    {
-      number: "Episode 26",
-      tittle: "",
-      information: "Self-driving cars and the road to autonomy.",
-      img: "../img/episodeTwentySix.jpg",
-      imgCell: "../img/episodeTwentySixCell.jpg",
-      url: "https://www.youtube.com/watch?v=q-F5J0Axtw0",
-      year: "2024"
-    },
-    {
-      number: "Episode 27",
-      tittle: "",
-      information: "How cloud computing reshapes businesses today.",
-      img: "../img/episodeTwentySeven.jpg",
-      imgCell: "../img/episodeTwentySevenCell.jpg",
-      url: "https://www.youtube.com/watch?v=s2Hu8kLnHxA",
-      year: "2024"
-    },
-    {
-      number: "Episode 28",
-      tittle: "",
-      information: "Exploring AI-generated art and its implications.",
-      img: "../img/episodeTwentyEight.jpg",
-      imgCell: "../img/episodeTwentyEightCell.jpg",
-      url: "https://www.youtube.com/watch?v=y8-S06jA9Mc",
-      year: "2024"
-    },
-    {
-      number: "Episode 29",
-      tittle: "",
-      information: "Tech for good: innovations solving global challenges.",
-      img: "../img/episodeTwentyNine.jpg",
-      imgCell: "../img/episodeTwentyNineCell.jpg",
-      url: "https://www.youtube.com/watch?v=fNzfxBqqmFY",
-      year: "2025"
-    },
-    {
-      number: "Episode 30",
-      tittle: "",
-      information: "Exploring brain-computer interfaces and their future.",
-      img: "../img/episodeThirty.jpg",
-      imgCell: "../img/episodeThirtyCell.jpg",
-      url: "https://www.youtube.com/watch?v=3JHvsyJvMk4",
-      year: "2025"
-    },
-    {
-      number: "Episode 31",
-      tittle: "",
-      information: "Digital twins: virtual replicas of the real world.",
-      img: "../img/episodeThirtyOne.jpg",
-      imgCell: "../img/episodeThirtyOneCell.jpg",
-      url: "https://www.youtube.com/watch?v=yM2BQFj7Gmc",
-      year: "2025"
-    },
-    {
-      number: "Episode 32",
-      tittle: "",
-      information: "How AI is enhancing creative processes in music and film.",
-      img: "../img/episodeThirtyTwo.jpg",
-      imgCell: "../img/episodeThirtyTwoCell.jpg",
-      url: "https://www.youtube.com/watch?v=Sm2Ad2X7pKw",
-      year: "2025"
     },
     {
       number: "Episode 33",
@@ -299,6 +155,8 @@ let global = {
       year: "2025"
     }
   ],
+
+
 
 
 i:0,
